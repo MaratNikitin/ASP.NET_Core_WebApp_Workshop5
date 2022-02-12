1f409371b2fae207302ba43dcd7219a52497ecc0
Scott Holmes <scott.holmes44@gmail.com>
2022-02-11 21:27:46 -0700

    Added Profile controller, manager and index page
    Also added profile to the navbar for authenticated users

---
+++ MVC_TravelExperts/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExpertsData;
+using TravelExpertsData.Models;
+
+/*
+* This is the controller responsible for updating a users data once they are logged in
+* Author: Scott Holmes
+* SAIT, PROJ 207 - Threaded Project
+* When: February 2022
+*/
+
+namespace MVC_TravelExperts.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // When a user clicks to view their profile
+        public IActionResult Index()
+        {
+            int custId = (int)HttpContext.Session.GetInt32("CurrentCustomer");
+            Customer currentCustomer = ProfileManager.GetCustomerByID(custId); // Get the customer object using their cust id
+            return View(currentCustomer);
+        }
+    }
+}
